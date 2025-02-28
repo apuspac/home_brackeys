2628e0265771404759fbdf850f95b26f995c2d0c
apuspac <21313205+apuspac@users.noreply.github.com>
2025-03-01 08:13:40 +0900

    #19 add: tutorialを繰り返した時に、矢印がでるように。

--- scripts/title.gd
+++ scripts/title.gd
@@ -1,11 +1,18 @@
 extends Control
 
+@onready var arrow_node: Control = $Arrow
+
 func _ready() -> void:
     var start_menu = get_node("MarginContainer/VBoxContainer").get_children()
     start_menu[0].start_pressed.connect(self._on_start_pressed)
     start_menu[1].pressed.connect(self._on_quit_pressed)
 
     start_menu[0].grab_focus()
+
+    if SceneManager.tutorial_count > 2:
+        arrow_node.visible = true
+    else:
+        arrow_node.visible = false
 
 func _on_start_pressed(key) -> void:
     if key == "right":
